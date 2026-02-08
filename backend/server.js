a30991add567353b3d4190597a98e8f14d310f79
kushalkambar5 <kushalkambar07@gmail.com>
2026-02-08 13:05:59 +0530

    aws errors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Start server
-app.listen(PORT, () => {
+app.listen(5000, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
